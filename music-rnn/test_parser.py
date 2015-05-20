0323f5c855e6651cf5d4f2889987bec9b5a2af88
AndreaDellera <andrea.delle93@gmail.com>
2015-05-20 12:42:09 +0200

    added multiple traing and validator

--- music-rnn/test_parser.py
+++ music-rnn/test_parser.py
@@ -2,11 +2,12 @@ import xml.etree.ElementTree as ET
 import glob
 from myBackProp import myBackpropTrainer
 
-# from pybrain.supervised.trainers import BackpropTrainer
 from pybrain.datasets import SupervisedDataSet
 from pybrain.tools.shortcuts import buildNetwork
 from pybrain.structure import SigmoidLayer, LinearLayer
 import matplotlib.pyplot as mpl
+
+from pybrain.tools.validation import Validator
 
 
 get_bin = lambda x: x >= 0 and str(bin(x))[2:] or "-" + str(bin(x))[3:]
@@ -91,66 +92,107 @@ def main():
     # list of all train files
     files = [glob.glob("../files/train/*.xml")]
     codecs = []
+    i = 0
     # extracting all the notes
     for file in files[0]:
+        codecs.append([])
         print "\nfile: " + file
         tree = ET.parse(file)
         notes = [Note(note, division, step_time) for note in tree.findall('.//note')]
         for note in notes:
-            codecs.append(note.encode())
+            codecs[i].append(note.encode())
+        i += 1
+
 
     # list of all test files
     files = [glob.glob("../files/test/*.xml")]
     tests = []
+    i = 0
     # extracting all the notes
     for file in files[0]:
+        tests.append([])
         print "\nfile: " + file
         tree = ET.parse(file)
         notes = [Note(note, division, step_time) for note in tree.findall('.//note')]
         for note in notes:
-            tests.append(note.encode())
+            tests[i].append(note.encode())
+        i += 1
 
     # creating the datasets
-    ds = SupervisedDataSet(11, 11)
-    dstest = SupervisedDataSet(11, 11)
+    ds = []
+    dstest = []
 
-    # creating the network
-    net = buildNetwork(ds.indim, 6, 6, ds.outdim, recurrent=True, outclass=LinearLayer, hiddenclass=SigmoidLayer)
-    trainer = myBackpropTrainer(net, dataset=ds, learningrate = 0.01, momentum = 0.99) # if verbose == True then print "Total error:", errors / ponderation
 
     # adding data to the ds
-    for i in range(0, len(codecs) - 2, 1):
-        ds.addSample((codecs[i][0], codecs[i][1], codecs[i][2], codecs[i][3], codecs[i][4], codecs[i][5], codecs[i][6],
-                      codecs[i][7], codecs[i][8], codecs[i][9], codecs[i][10])
-                     ,
-                     (codecs[i + 1][0], codecs[i + 1][1], codecs[i + 1][2], codecs[i + 1][3], codecs[i + 1][4],
-                      codecs[i + 1][5], codecs[i + 1][6], codecs[i + 1][7], codecs[i + 1][8], codecs[i + 1][9],
-                      codecs[i + 1][10])
-                     )
-    # print ds
+    for j in range(len(codecs)):
+        ds.append(SupervisedDataSet(11, 11))
+        for i in range(0, len(codecs[j]) - 2, 1):
+            ds[j].addSample((codecs[j][i][0], codecs[j][i][1], codecs[j][i][2], codecs[j][i][3], codecs[j][i][4],
+                             codecs[j][i][5], codecs[j][i][6],
+                             codecs[j][i][7], codecs[j][i][8], codecs[j][i][9], codecs[j][i][10])
+                            ,
+                            (codecs[j][i + 1][0], codecs[j][i + 1][1], codecs[j][i + 1][2], codecs[j][i + 1][3],
+                             codecs[j][i + 1][4],
+                             codecs[j][i + 1][5], codecs[j][i + 1][6], codecs[j][i + 1][7], codecs[j][i + 1][8],
+                             codecs[j][i + 1][9],
+                             codecs[j][i + 1][10])
+                            )
+
 
     # adding data to the dstest
-    for i in range(0, len(tests) - 2, 1):
-        dstest.addSample((tests[i][0], tests[i][1], tests[i][2], tests[i][3], tests[i][4], tests[i][5], tests[i][6],
-                          tests[i][7], tests[i][8], tests[i][9], tests[i][10]),
-                         (tests[i + 1][0], tests[i + 1][1], tests[i + 1][2], tests[i + 1][3], tests[i + 1][4],
-                          tests[i + 1][5], tests[i + 1][6], tests[i + 1][7], tests[i + 1][8], tests[i + 1][9],
-                          tests[i + 1][10], )
-                         )
-    # print dstest
+    for j in range(len(tests)):
+        dstest.append(SupervisedDataSet(11, 11))
+        for i in range(0, len(tests[j]) - 2, 1):
+            dstest[j].addSample((tests[j][i][0], tests[j][i][1], tests[j][i][2], tests[j][i][3], tests[j][i][4],
+                                 tests[j][i][5], tests[j][i][6],
+                                 tests[j][i][7], tests[j][i][8], tests[j][i][9], tests[j][i][10]),
+                                (tests[j][i + 1][0], tests[j][i + 1][1], tests[j][i + 1][2], tests[j][i + 1][3],
+                                 tests[j][i + 1][4],
+                                 tests[j][i + 1][5], tests[j][i + 1][6], tests[j][i + 1][7], tests[j][i + 1][8],
+                                 tests[j][i + 1][9],
+                                 tests[j][i + 1][10], )
+                                )
 
+    # creating the network
+    net = buildNetwork(ds[0].indim, 6, ds[0].outdim, recurrent=True, outclass=LinearLayer, hiddenclass=SigmoidLayer)
+    # if verbose == True then print "Total error:", MSE / ponderation
+    trainer = myBackpropTrainer(net, learningrate=0.01, momentum=0.99)
+
+
+    x = []
     print "start training"
-    x = trainer.trainOnDataset(ds, 100)
+    for i in range(len(ds)):
+        x += trainer.trainOnDataset(ds[i], 100)
+
     print "finish training"
 
-    print "start testing"
-    y = trainer.testOnData(dstest, verbose=False)
-    print "finish testing"
 
-    mpl.plot(range(len(x)), x)
-    mpl.show()
+    mse = Validator()
+    print len(dstest)
+    for i in range(len(dstest)):
+        activations = []
+        targets = []
+        for inp, out in dstest[i]:
+            activations.append(net.activate(inp))
+            targets.append(out)
+            print net.activate(inp), '\n', out
+        targets = [out for inp, out in dstest[i]]
+        print i, "                  ", mse.MSE(activations, targets)
 
-    print y
+
+
+    # y = []
+    # print "start testing"
+    # for i in range(len(dstest)):
+    #     y += trainer.testOnData(dstest[i], verbose=False)
+    #     print "testing on ", i, "\n"
+    # print "finish testing"
+
+    # mpl.plot(range(len(x)), x)
+    # mpl.show()
+
+    # mpl.plot(range(len(y)), y)
+    # mpl.show()
 
 
 if __name__ == "__main__":
