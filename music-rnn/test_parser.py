45510c98cfaba85678f3263e950a33b26c4e6ed0
AndreaDellera <andrea.delle93@gmail.com>
2015-05-20 22:05:21 +0200

    no hidden layer

--- music-rnn/test_parser.py
+++ music-rnn/test_parser.py
@@ -4,7 +4,7 @@ from myBackProp import myBackpropTrainer
 
 from pybrain.datasets import SupervisedDataSet
 from pybrain.tools.shortcuts import buildNetwork
-from pybrain.structure import SigmoidLayer, LinearLayer
+from pybrain.structure import SigmoidLayer, LinearLayer, RecurrentNetwork
 import matplotlib.pyplot as mpl
 
 from pybrain.tools.validation import Validator
@@ -127,15 +127,13 @@ def main():
     for j in range(len(codecs)):
         ds.append(SupervisedDataSet(11, 11))
         for i in range(0, len(codecs[j]) - 2, 1):
-            ds[j].addSample((codecs[j][i][0], codecs[j][i][1], codecs[j][i][2], codecs[j][i][3], codecs[j][i][4],
-                             codecs[j][i][5], codecs[j][i][6],
-                             codecs[j][i][7], codecs[j][i][8], codecs[j][i][9], codecs[j][i][10])
+            ds[j].appendLinked((codecs[j][i][0], codecs[j][i][1], codecs[j][i][2], codecs[j][i][3], codecs[j][i][4],
+                             codecs[j][i][5], codecs[j][i][6], codecs[j][i][7], codecs[j][i][8], codecs[j][i][9],
+                             codecs[j][i][10])
                             ,
                             (codecs[j][i + 1][0], codecs[j][i + 1][1], codecs[j][i + 1][2], codecs[j][i + 1][3],
-                             codecs[j][i + 1][4],
-                             codecs[j][i + 1][5], codecs[j][i + 1][6], codecs[j][i + 1][7], codecs[j][i + 1][8],
-                             codecs[j][i + 1][9],
-                             codecs[j][i + 1][10])
+                             codecs[j][i + 1][4], codecs[j][i + 1][5], codecs[j][i + 1][6], codecs[j][i + 1][7],
+                             codecs[j][i + 1][8], codecs[j][i + 1][9], codecs[j][i + 1][10])
                             )
 
 
@@ -143,57 +141,62 @@ def main():
     for j in range(len(tests)):
         dstest.append(SupervisedDataSet(11, 11))
         for i in range(0, len(tests[j]) - 2, 1):
-            dstest[j].addSample((tests[j][i][0], tests[j][i][1], tests[j][i][2], tests[j][i][3], tests[j][i][4],
-                                 tests[j][i][5], tests[j][i][6],
-                                 tests[j][i][7], tests[j][i][8], tests[j][i][9], tests[j][i][10]),
+            dstest[j].appendLinked((tests[j][i][0], tests[j][i][1], tests[j][i][2], tests[j][i][3], tests[j][i][4],
+                                 tests[j][i][5], tests[j][i][6], tests[j][i][7], tests[j][i][8], tests[j][i][9],
+                                 tests[j][i][10]),
                                 (tests[j][i + 1][0], tests[j][i + 1][1], tests[j][i + 1][2], tests[j][i + 1][3],
-                                 tests[j][i + 1][4],
-                                 tests[j][i + 1][5], tests[j][i + 1][6], tests[j][i + 1][7], tests[j][i + 1][8],
-                                 tests[j][i + 1][9],
-                                 tests[j][i + 1][10], )
+                                 tests[j][i + 1][4], tests[j][i + 1][5], tests[j][i + 1][6], tests[j][i + 1][7],
+                                 tests[j][i + 1][8], tests[j][i + 1][9], tests[j][i + 1][10])
                                 )
+    # import pdb; pdb.set_trace()
 
     # creating the network
+    # without hidden layers the network works properly
     net = buildNetwork(ds[0].indim, 6, ds[0].outdim, recurrent=True, outclass=LinearLayer, hiddenclass=SigmoidLayer)
     # if verbose == True then print "Total error:", MSE / ponderation
     trainer = myBackpropTrainer(net, learningrate=0.01, momentum=0.99)
 
-
     x = []
     print "start training"
     for i in range(len(ds)):
-        x += trainer.trainOnDataset(ds[i], 100)
-
+        x += trainer.trainOnDataset(ds[i], 200)
     print "finish training"
 
-
-    mse = Validator()
-    print len(dstest)
-    for i in range(len(dstest)):
-        activations = []
-        targets = []
-        for inp, out in dstest[i]:
-            activations.append(net.activate(inp))
-            targets.append(out)
-            print net.activate(inp), '\n', out
-        targets = [out for inp, out in dstest[i]]
-        print i, "                  ", mse.MSE(activations, targets)
-
-
-
-    # y = []
-    # print "start testing"
+    # mse = Validator()
+    # print len(dstest)
     # for i in range(len(dstest)):
-    #     y += trainer.testOnData(dstest[i], verbose=False)
-    #     print "testing on ", i, "\n"
-    # print "finish testing"
+    #     activations = []
+    #     targets = []
+    #     for inp, out in dstest[i]:
+    #         activations.append(net.activate(inp))
+    #         targets.append(out)
+    #         # print net.activate(inp), '\n', out
+    #     targets = [out for inp, out in dstest[i]]
+    #     print i, "                  ", mse.MSE(activations, targets)
 
+    # output's decode
+
+    # creation of the new music xml
+    noteValue = durationValue = stepValue = alterValue = octaveValue = 0
+    print noteValue, durationValue, stepValue, alterValue, octaveValue
+
+    y = []
+    print "start testing"
+    for i in range(len(dstest)):
+        y += trainer.testOnData(dstest[i], verbose=True)
+        print "testing on ", i, "\n"
+    print "finish testing"
+    #
     # mpl.plot(range(len(x)), x)
     # mpl.show()
 
-    # mpl.plot(range(len(y)), y)
-    # mpl.show()
+    mpl.plot(range(len(y)), y)
+    mpl.show()
+    # print net['in']
+    # print net['hidden0']
+    # print net['out']
 
+    net.reset()
 
 if __name__ == "__main__":
     main()
